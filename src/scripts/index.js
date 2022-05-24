92bcf36ae2a41645008b5caa014cab7c5ef50f97
M. Fajar Aditya <mfajaraditya13@gmail.com>
2022-05-24 16:40:05 +0800

    Fixed app-bar & footer-bar.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,7 @@
 import '../styles/main.css';
 import '../styles/responsive.css';
-import '../scripts/components/app-bar';
+import '../styles/components/app-bar.css';
+import '../styles/components/footer-bar.css';
+import '../scripts/components/app-bar.js';
+import '../scripts/components/footer-bar.js';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
